f25bc42ea23cdcf98105b38a3c551da28763e03c
Eric Chen <eric.chen@wsu.edu>
2017-05-06 15:42:06 -0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿// Eric Chen 11381898
+// CptS 322 HW1
+// Sample input: 55 22 77 88 11 22 44 77 55 99 22
+
+using System;
+
+class hw1
+{
+    class Node
+    {
+    }
+
+    class BinarySearchTree
+    {
+    }
+}
